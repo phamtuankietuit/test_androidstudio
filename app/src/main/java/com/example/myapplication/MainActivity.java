9ee99e099956645559a12b58caf0dd1145b81b3d
kit <kietphamkb2@gmail.com>
2023-03-23 23:50:47 +0700

    commit 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText_username = (EditText) findViewById(R.id.editText_username);
+        EditText editText_password = (EditText) findViewById(R.id.editText_password);
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String a = editText_username.getText().toString();
+                String b = editText_password.getText().toString();
+
+                if (a.equals("admin") && (b.equals("admin")))
+                {
+                    Toast.makeText(MainActivity.this, "Thành công", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
     }
+
+
+
+
+
+
 }
